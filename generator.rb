bb38283e60cb8c4ce468852cbddb416521c721e7
Victor Martinez <vicmargar@gmail.com>
2013-03-20 12:41:36 +0000

    Added more complexity with hosts and rooms
    bundle exec ./tester.rb 2013-03-20 2013-03-24 1

--- generator.rb
+++ generator.rb
@@ -10,23 +10,47 @@ $client.query("DROP DATABASE IF EXISTS killer")
 $client.query("CREATE DATABASE killer")
 $client.query("USE killer")
 
-def kill(count, days)
+def kill(params)
+  hosts          = params[:hosts]
+  rooms_per_host = params[:rooms_per_host]
+  days           = params[:days]
+
   $client.query("DROP TABLE IF EXISTS killer")
-  $client.query("CREATE TABLE killer (id INTEGER NOT NULL AUTO_INCREMENT, ref INTEGER NOT NULL, date DATE NOT NULL, price INTEGER NOT NULL, available INTEGER NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB;")
-  count.times do |ref|
-    price = rand(20..120)
-    available = rand(1..5)
+  $client.query("DROP TABLE IF EXISTS hosts")
+  $client.query("DROP TABLE IF EXISTS rooms")
 
-    values = []
-    days.times do |i|
-      date = Date.today + i
-      date_price = price - rand(-10..10)
-      date_available = available - rand(0..available)
+  $client.query("CREATE TABLE hosts (id INTEGER NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB;")
+  $client.query("CREATE TABLE rooms (id INTEGER NOT NULL, host_id INTEGER NOT NULL, capacity INTEGER NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB;")
+  $client.query("CREATE TABLE killer (id INTEGER NOT NULL AUTO_INCREMENT, host_id INTEGER NOT NULL, room_id INTEGER NOT NULL, date DATE NOT NULL, price INTEGER NOT NULL, available INTEGER NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB;")
 
-      values << "(#{ref}, '#{date}', #{date_price}, #{date_price})"
+
+  host_values   = []
+  room_values   = []
+  killer_values = []
+
+  hosts.times do |h|
+    host_id = h + 1
+    host_values << "(#{host_id})"
+
+    rooms_per_host.times do |r|
+      room_id = (host_id * rooms_per_host) - (rooms_per_host - r - 1)
+      room_capacity = rand(2..4)
+      room_price = rand(20..120)
+      room_values << "(#{room_id}, #{host_id}, #{room_capacity})"
+
+      days.times do |i|
+        date = Date.today + i
+        date_price = room_price - rand(-10..10)
+        date_available = room_capacity - rand(0..room_capacity)
+        killer_values << "(#{host_id}, #{room_id}, '#{date}', #{date_price}, #{date_available})"
+      end
     end
-    $client.query("INSERT INTO killer (ref, date, price, available) VALUES#{values.join(',')}")
   end
+
+  $client.query("INSERT INTO hosts (id) VALUES#{host_values.join(',')}")
+  $client.query("INSERT INTO rooms (id, host_id, capacity) VALUES#{room_values.join(',')}")
+  $client.query("INSERT INTO killer (host_id, room_id, date, price, available) VALUES#{killer_values.join(',')}")
 end
 
-kill(1000, 1000)
+# kill(hosts: 1000, rooms_per_host:2, days: 365)
+kill(hosts: 2, rooms_per_host:2, days: 5)
--- tester.rb
+++ tester.rb
@@ -6,33 +6,39 @@ require 'benchmark'
 
 Bundler.require
 
-$client = Mysql2::Client.new(:username => 'root')
+$client = Mysql2::Client.new(:username => 'hst')
 $client.query("USE killer")
 
-if ARGV.length == 2
+if ARGV.length == 3
   start_date = Date.parse(ARGV[0])
   end_date = Date.parse(ARGV[1])
+  guests = ARGV[2].to_i
 else
   start_date = Date.today
   end_date = Date.today + 45
+  guests = 1
 end
 
 time = Benchmark.realtime do
-  $results = $client.query <<EOF
-    SELECT ref, SUM(price) AS total FROM killer
+query =<<EOF
+    SELECT room_id, SUM(price) AS total, MIN(available) as available FROM killer
     WHERE
       date BETWEEN "#{start_date}" AND "#{end_date}"
-    GROUP BY ref
+    GROUP BY room_id
+    HAVING
+      MIN(available) >= #{guests}
     ORDER BY total
 EOF
+puts query
+$results = $client.query query
 end
 puts "Query took #{time}s"
-puts "------------------"
-puts "|    Ref | Total |"
-puts "------------------"
+puts "------------------------------"
+puts "| RoomId | Total | Available |"
+puts "------------------------------"
 
 $results.each(:symbolize_keys => true) do |row|
-  puts "| #{row[:ref].to_s.rjust 6} | #{row[:total].to_i.to_s.rjust 5} |"
+  puts "| #{row[:room_id].to_s.rjust 6} | #{row[:total].to_i.to_s.rjust 5} | #{row[:available].to_i.to_s.rjust 9} |"
 end
 
-puts "------------------"
+puts "------------------------------"
